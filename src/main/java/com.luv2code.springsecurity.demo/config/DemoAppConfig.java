cf95ef627a64ce745291eefc378ee4a62d00cd2f
RLA04 <RLA04@ServiceDesks-MacBook-Pro.local>
2019-11-13 08:49:02 +0100

    initial commit

---
+++ src/main/java/com.luv2code.springsecurity.demo/config/DemoAppConfig.java
@@ -0,0 +1,59 @@
+package com.luv2code.springsecurity.demo.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.luv2code.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
+public class DemoAppConfig {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource securityDataSource() {
+
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exc) {
+            throw new RuntimeException(exc);
+        }
+
+        logger.info(">>>> jdbc.url=" +env.getProperty("jdbc.url"));
+        logger.info(">>>> jdbc.user=" +env.getProperty("jdbc.user"));
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+        securityDataSource.setInitialPoolSize(Integer.parseInt(env.getProperty("connection.pool.initialPoolSize")));
+        securityDataSource.setMinPoolSize(Integer.parseInt(env.getProperty("connection.pool.minPoolSize")));
+        securityDataSource.setMaxPoolSize(Integer.parseInt(env.getProperty("connection.pool.maxPoolSize")));
+        securityDataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("connection.pool.maxIdeTime")));
+        return securityDataSource;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
